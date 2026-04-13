ccda8a90915a56e7c3f486a6eb0a747a137c9188
Andy <61007039+andy-1977@users.noreply.github.com>
2026-04-13 19:37:44 +0100

    Improve crawlability of images on product pages (#7567)
    * Feature Improvement To Images On The Product Pages
    
    Improvement to the images on the main product pages.
    
    Currently if you run a lighthouse page insights test on a product page, under the SEO part, you'll see that the images get flagged as "Links are not crawlable" with the main product image and additional product images (if there are any) getting flagged for this as well. You'll also see this as well "<a href="javascript:void(0);" onclick="openModal('imageModalPrimary')">"
    
    The code change that I've suggested fixes this, and the warning will go.
    
    The images are also now crawlable as well.

--- includes/templates/responsive_classic/modalboxes/tpl_image_additional.php
+++ includes/templates/responsive_classic/modalboxes/tpl_image_additional.php
@@ -2,27 +2,37 @@
 /**
  * @copyright Copyright 2003-2025 Zen Cart Development Team
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: torvista 2025 Mar 23 Modified in v2.2.0 $
+ * @version $Id: DrByte 2025 May 15 Modified in v2.2.0 $
  */
 // Display the modal and the original image. Contained in the wrapping image-grid div.
 ?>
-<!-- Modal -->
+
+<!-- Modal for additional image -->
 <div id="<?= $modal_id ?>" class="imgmodal">
     <div id="<?= $modal_content_id ?>" class="imgmodal-content">
         <div onclick="closeModal('<?= $modal_id ?>')">
-            <?= zen_image($image['products_image_large'], $image['products_name'], '', '', 'class="centered-image"') ?>
+            <!-- Large image inside modal -->
+            <?= zen_image(
+                $image['products_image_large'],
+                $image['products_name'],
+                '',
+                '',
+                'class="centered-image"'
+            ); ?>
             <div class="imgmodal-close"><i class="fa-solid fa-circle-xmark"></i></div>
-            <div class="center"><?= $image['products_name'] ?></div>
-            <?php /*
-            <div class="imgLink center"><?= TEXT_CLOSE_WINDOW_IMAGE ?></div>
-            */ ?>
+            <div class="center"><?= htmlspecialchars($image['products_name'], ENT_QUOTES, 'UTF-8'); ?></div>
         </div>
     </div>
 </div>
+
+<!-- Thumbnail for additional image -->
 <div class="back">
-    <a id="<?= $modal_link_id ?>" <?= $modal_link_attributes ?>><?= $modal_link_img ?>
-    <?php /*
-    <div class="imgLink center"><?= TEXT_CLICK_TO_ENLARGE ?></div>
-     */ ?>
+    <a id="<?= $modal_link_id ?>"
+       href="<?= htmlspecialchars($large_image, ENT_QUOTES, 'UTF-8'); ?>"
+       onclick="openModal('<?= $modal_id ?>'); return false;"
+       title="<?= htmlspecialchars(TEXT_CLICK_TO_ENLARGE . ' ' . $image['products_name'], ENT_QUOTES, 'UTF-8'); ?>">
+       
+        <?= $modal_link_img; ?>
+        
     </a>
 </div>
