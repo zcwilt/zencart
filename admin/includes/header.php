088f0b322ce486c672ca24633f31df5a1f16eab3
Chris Brown <drbyte@zen-cart.com>
2026-04-18 19:59:26 -0400

    Handle regression of Admin UpperMenu observer
    Prior-version observers will have to check the array for evidence of the new structure, but can now override much more content.
    (ie: look for `'nav-admin-home-link'` element in the array, or absence of an `'a'` element in the top of the array.)
    
    Ref #7619

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -71,37 +71,71 @@ $admin_ip = $_SERVER['REMOTE_ADDR'];
 $admin_host = gethostname();
 $admin_tz = date_default_timezone_get();
 $admin_locale = setlocale(LC_TIME, 0);
+zen_define_default('ADMIN_NAV_TIMEZONE_FORMAT', '(%z)');
 
-$upperMenuArray = [
-        [ 'a' => zen_href_link(FILENAME_DEFAULT), 'title' => HEADER_TITLE_TOP ],
-        [ 'a' => zen_catalog_href_link(FILENAME_DEFAULT), 'title' => HEADER_TITLE_ONLINE_CATALOG, 'params' => 'target="_blank" rel="noopener"' ],
-        [ 'a' => 'https://www.zen-cart.com/forum', 'title' => HEADER_TITLE_SUPPORT_SITE ],
-        [ 'a' => zen_href_link(FILENAME_SERVER_INFO), 'title' => HEADER_TITLE_VERSION ],
-        [ 'a' => zen_href_link(FILENAME_ADMIN_ACCOUNT), 'title' => HEADER_TITLE_ACCOUNT ],
-        [ 'a' => zen_href_link(FILENAME_LOGOFF), 'title' => HEADER_TITLE_LOGOFF ],
+// Prepare menu items for upper-right nav bar, allowing observers to modify via NOTIFY_ADMIN_HEADER_UPPERMENU
+$upperMenuArray = [];
+$upperMenuArray['nav-search-orders-form'] = [
+    'enabled' => true,
 ];
+$upperMenuArray['nav-search-customers-form'] = [
+    'enabled' => false,
+];
+$upperMenuArray['nav-goto-category-form'] = [
+    'enabled' => false,
+];
+$upperMenuArray['nav-current-time'] = [
+    'enabled' => true,
+];
+$upperMenuArray['nav-admin-home-link'] = [
+    'a' => zen_href_link(FILENAME_DEFAULT),
+    'title' => HEADER_TITLE_TOP,
+    'icon' => 'fa-home',
+    'enabled' => true,
+];
+$upperMenuArray['nav-storefront-link'] = [
+    'a' => zen_catalog_href_link(FILENAME_DEFAULT),
+    'title' => HEADER_TITLE_ONLINE_CATALOG,
+    'icon' => 'fa-store',
+    'enabled' => true,
+];
+$upperMenuArray['version-indicator-icon'] = [
+    // Note: This is just the icon in the nav bar, the actual link and dropdown content is built in the header HTML below to allow for dynamic version checking content
+    // NOTE: This is ALWAYS enabled on the server-info page.
+    'enabled' => true,
+];
+$upperMenuArray['admin-account-link'] = [
+    'a' => zen_href_link(FILENAME_ADMIN_ACCOUNT),
+    'title' => HEADER_TITLE_ACCOUNT,
+    'icon' => 'fa-user',
+    'enabled' => true,
+];
+$upperMenuArray['version-info-link'] = [
+    'a' => zen_href_link(FILENAME_SERVER_INFO),
+    'title' => HEADER_TITLE_VERSION,
+    'icon' => 'fa-server',
+    'enabled' => true,
+];
+$upperMenuArray['support-forum-link'] = [
+    'a' => "https://www.zen-cart.com/forum",
+    'title' => HEADER_TITLE_SUPPORT_SITE,
+    'icon' => 'fa-info-circle',
+    'enabled' => true,
+];
+$upperMenuArray['logoff'] = [
+    'a' => zen_href_link(FILENAME_LOGOFF),
+    'title' => HEADER_TITLE_LOGOFF,
+    'icon' => 'fa-sign-out',
+    'enabled' => true,
+];
+
 $upperMenuOverrideArray = '';
-$zco_notifier->notify('NOTIFY_ADMIN_HEADER_UPPERMENU', $upperMenuArray, $upperMenuOverrideArray);
+$plugin_menu_items = [];
+$zco_notifier->notify('NOTIFY_ADMIN_HEADER_UPPERMENU', $upperMenuArray, $upperMenuOverrideArray, $plugin_menu_items);
 if (!empty($upperMenuOverrideArray) && is_array($upperMenuOverrideArray)) {
     $upperMenuArray = $upperMenuOverrideArray;
 }
-// Filter out core links to dynamically append plugin-added links
-$core_links = [
-        zen_href_link(FILENAME_DEFAULT),
-        zen_catalog_href_link(FILENAME_DEFAULT),
-        'https://www.zen-cart.com/forum',
-        zen_href_link(FILENAME_SERVER_INFO),
-        zen_href_link(FILENAME_ADMIN_ACCOUNT),
-        zen_href_link(FILENAME_LOGOFF)
-];
-$plugin_menu_items = [];
-foreach ($upperMenuArray as $menuItem) {
-    if (!in_array($menuItem['a'], $core_links)) {
-        $plugin_menu_items[] = $menuItem;
-    }
-}
 ?>
-
     <nav class="navbar navbar-inverse navbar-fixed-top top-tier">
         <div class="container-fluid">
             <div class="navbar-header">
@@ -123,8 +157,8 @@ foreach ($upperMenuArray as $menuItem) {
 
             <div class="collapse navbar-collapse" id="top-bar-collapse">
                 <ul class="nav navbar-nav navbar-left">
-                    <?php if (check_page(FILENAME_ORDERS, '')) { ?>
-                    <li class="hidden-xs">
+                    <?php if (($upperMenuArray['nav-search-orders-form']['enabled'] ?? false) && check_page(FILENAME_ORDERS, '')) { ?>
+                    <li class="hidden-xs" id="nav-search-orders">
                         <?= zen_draw_form('order_search', FILENAME_ORDERS, '', 'get', 'class="navbar-form"', true) ?>
                         <div class="form-group header-search">
                         <?= zen_draw_input_field('oID', '', 'id="oIDsearch" class="form-control" placeholder="' . HEADER_TEXT_SEARCH_ORDERS . '"', false, 'search') ?>
@@ -134,8 +168,8 @@ foreach ($upperMenuArray as $menuItem) {
                     </li>
                     <?php } ?>
 
-                    <?php if (false && check_page(FILENAME_CUSTOMERS, '')) { ?>
-                    <li class="hidden-xs">
+                    <?php if (($upperMenuArray['nav-search-customers-form']['enabled'] ?? false) && check_page(FILENAME_CUSTOMERS, '')) { ?>
+                    <li class="hidden-xs" id="nav-search-customers">
                         <?= zen_draw_form('customer_search', FILENAME_CUSTOMERS, '', 'get', 'class="navbar-form"', true); ?>
                         <div class="form-group header-search">
                         <?= zen_draw_input_field('search', '', 'id="cIDsearch" class="form-control" placeholder="' . HEADER_TEXT_SEARCH_CUSTOMERS . '"', false, 'search'); ?>
@@ -144,8 +178,8 @@ foreach ($upperMenuArray as $menuItem) {
                     </li>
                     <?php } ?>
 
-                    <?php if (false && check_page(FILENAME_CATEGORY_PRODUCT_LISTING, '')) { ?>
-                    <li class="hidden-xs">
+                    <?php if (($upperMenuArray['nav-goto-category-form']['enabled'] ?? false) && check_page(FILENAME_CATEGORY_PRODUCT_LISTING, '')) { ?>
+                    <li class="hidden-xs" id="nav-goto-category">
                         <?= zen_draw_form('goto', FILENAME_CATEGORY_PRODUCT_LISTING, '', 'get', 'class="navbar-form"') ?>
                         <div class="form-group header-search goto-category">
                             <small class="text-muted"><?= HEADER_TEXT_JUMP_TO_CATEGORY ?><br></small>
@@ -156,22 +190,25 @@ foreach ($upperMenuArray as $menuItem) {
                     <?php } ?>
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
-                    <li>
+                    <?php if ($upperMenuArray['nav-current-time']['enabled'] ?? false) { ?>
+                    <li id="nav-current-time">
                         <div class="currentTime">
                         <?= mb_convert_encoding($zcDate->output(ADMIN_NAV_DATE_TIME_FORMAT, time()), 'UTF-8') ?>
-                        <?php zen_define_default('ADMIN_NAV_TIMEZONE_FORMAT', '(%z)') ?>
                         <br><small id="nav-timezone"><?= date_default_timezone_get() ?> <?= $zcDate->output(ADMIN_NAV_TIMEZONE_FORMAT, time()) ?></small>
                         </div>
                     </li>
+                    <?php } ?>
 
-                    <li class="hidden-xs">
+                    <?php if ($upperMenuArray['nav-admin-home-link']['enabled'] ?? false) { ?>
+                    <li class="hidden-xs" id="nav-admin-home">
                         <a href="<?= zen_href_link(FILENAME_DEFAULT) ?>" title="<?= HEADER_TITLE_TOP ?>">
                             <i class="fa fa-home"></i> <?= HEADER_TITLE_TOP ?>
                         </a>
                     </li>
+                    <?php } ?>
 
                     <?php if (!empty($new_gv_queue_cnt)) { ?>
-                        <li>
+                        <li id="nav-gift-queue">
                             <a href="<?= zen_href_link(FILENAME_GV_QUEUE) ?>" title="<?= strip_tags(IMAGE_GIFT_QUEUE) ?>">
                                 <i class="fa fa-gift"></i>
                                 <span class="badge"><?= $new_gv_queue_cnt ?></span>
@@ -179,13 +216,16 @@ foreach ($upperMenuArray as $menuItem) {
                         </li>
                     <?php } ?>
 
-                    <li>
+                    <?php if ($upperMenuArray['nav-storefront-link']['enabled'] ?? false) { ?>
+                    <li id="nav-storefront">
                         <a href="<?= zen_catalog_href_link(FILENAME_DEFAULT) ?>" target="_blank" title="<?= HEADER_TITLE_ONLINE_CATALOG ?>" rel="noopener">
                             <i class="fa fa-store"></i> <?= HEADER_TITLE_ONLINE_CATALOG ?>
                         </a>
                     </li>
+                    <?php } ?>
 
-                    <li class="dropdown">
+                    <?php if (check_page(FILENAME_SERVER_INFO, '') && (basename($PHP_SELF) === FILENAME_SERVER_INFO . '.php' || $upperMenuArray['version-indicator-icon']['enabled'] ?? false)) { ?>
+                    <li class="dropdown" id="nav-version-info">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" title="<?= HEADER_TITLE_VERSION ?>">
                             <i id="versionCheckPill" class="fa fa-server"></i> <span class="visible-xs-inline"> <?= HEADER_TITLE_VERSION ?></span>
                             <span id="versionCheckNotifyBadge" class="badge-notify" style="display:none"></span>
@@ -204,9 +244,10 @@ foreach ($upperMenuArray as $menuItem) {
                             </li>
                         </ul>
                     </li>
+                    <?php } ?>
 
                     <?php if (!empty($languages_array)) { ?>
-                        <li class="dropdown">
+                        <li class="dropdown" id="nav-language-selector">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                                 <i class="fa fa-flag"></i> <span class="visible-xs-inline"> <?= HEADER_TEXT_LANGUAGES ?></span> <b class="caret"></b>
                             </a>
@@ -218,26 +259,31 @@ foreach ($upperMenuArray as $menuItem) {
                         </li>
                     <?php } ?>
 
-                    <li class="dropdown">
+                    <li class="dropdown" id="nav-user-menu">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                             <span class="user-avatar"></span>
                             <?= zen_output_string_protected(zen_get_admin_name($_SESSION['admin_id'])) ?>
                             <b class="caret"></b>
                         </a>
                         <ul class="dropdown-menu">
-                            <li><a href="<?= zen_href_link(FILENAME_ADMIN_ACCOUNT, '', 'NONSSL') ?>"><i class="fa fa-user"></i> <?= HEADER_TITLE_ACCOUNT ?></a></li>
-                            <li><a href="<?= zen_href_link(FILENAME_SERVER_INFO, '', 'NONSSL') ?>"><i class="fa fa-info-circle"></i> <?= HEADER_TITLE_VERSION ?></a></li>
-
-                            <?php if (!empty($plugin_menu_items)) { ?>
-                                <li class="divider"></li>
-                                <?php foreach ($plugin_menu_items as $item) { ?>
-                                <li <?= !empty($item['id']) ? 'id="' . $item['id'] . '"' : '' ?>  <?= !empty($item['li-class']) ? 'class="' . $item['li-class'] . '"' : '' ?>><a href="<?= $item['a'] ?>"
-                                        <?= $item['params'] ?? '' ?>><i class="fa <?= $item['icon'] ?? 'fa-plug' ?>"></i> <?= $item['title'] ?></a></li>
-                                <?php } ?>
+                            <?php if ($upperMenuArray['admin-account-link']['enabled'] ?? false) { ?>
+                            <li id="nav-account"><a href="<?= $upperMenuArray['admin-account-link']['a'] ?>"><i class="fa <?= $upperMenuArray['admin-account-link']['icon'] ?? 'fa-user' ?>"></i> <?= $upperMenuArray['admin-account-link']['title'] ?></a></li>
+                            <?php } ?>
+                            <?php if (check_page(FILENAME_SERVER_INFO, '') && $upperMenuArray['version-info-link']['enabled'] ?? false) { ?>
+                            <li id="nav-serverinfo"><a href="<?= $upperMenuArray['version-info-link']['a'] ?>"><i class="fa <?= $upperMenuArray['version-info-link']['icon'] ?? 'fa-info-circle' ?>"></i> <?= $upperMenuArray['version-info-link']['title'] ?></a></li>
                             <?php } ?>
 
+                        <?php if (!empty($plugin_menu_items)) { ?>
                             <li class="divider"></li>
-                            <li class="header-info-menu">
+                            <?php foreach ($plugin_menu_items as $item) { ?>
+                            <li <?= !empty($item['id']) ? 'id="' . $item['id'] . '" ' : '' ?><?= !empty($item['li-class']) ? 'class="' . $item['li-class'] . '"' : '' ?>>
+                                <a href="<?= $item['a'] ?>" <?= $item['params'] ?? '' ?>><i class="fa <?= $item['icon'] ?? 'fa-plug' ?>"></i> <?= $item['title'] ?></a>
+                            </li>
+                            <?php } ?>
+                        <?php } ?>
+
+                            <li class="divider"></li>
+                            <li class="header-info-menu" id="nav-my-ip-info">
                                 <span class="info-label"><?= HEADER_TEXT_IP_ADDRESS ?></span>
                                 <span class="info-val"><?= $admin_ip ?></span>
 
@@ -245,6 +291,7 @@ foreach ($upperMenuArray as $menuItem) {
                                 <span class="info-val"><?= $admin_host ?></span>
 
                                 <span class="info-label"><?= HEADER_TEXT_TIMEZONE ?></span>
+                                <span class="info-val"><?= mb_convert_encoding($zcDate->output(ADMIN_NAV_DATE_TIME_FORMAT, time()), 'UTF-8') ?></span>
                                 <span class="info-val"><?= $admin_tz ?></span>
 
                                 <span class="info-label"><?= HEADER_TEXT_LOCALE ?></span>
@@ -252,8 +299,12 @@ foreach ($upperMenuArray as $menuItem) {
                             </li>
 
                             <li class="divider"></li>
-                            <li><a href="https://www.zen-cart.com/forum"><i class="fa fa-info-circle"></i> <?= HEADER_TITLE_SUPPORT_SITE ?></a></li>
-                            <li><a href="<?= zen_href_link(FILENAME_LOGOFF, '', 'NONSSL') ?>"><i class="fa fa-sign-out"></i> <?= HEADER_TITLE_LOGOFF ?></a></li>
+                            <?php if ($upperMenuArray['support-forum-link']['enabled'] ?? false) { ?>
+                            <li id="nav-forum"><a href="<?= $upperMenuArray['support-forum-link']['enabled'] ?>"><i class="fa <?= $upperMenuArray['support-forum-link']['icon'] ?? 'fa-info-circle' ?>"></i> <?= $upperMenuArray['support-forum-link']['title'] ?></a></li>
+                            <?php } ?>
+                            <?php if ($upperMenuArray['logoff']['enabled'] ?? false) { ?>
+                                <li id="nav-logoff"><a href="<?= $upperMenuArray['logoff']['a'] ?>"><i class="fa <?= $upperMenuArray['logoff']['icon'] ?? 'fa-sign-out' ?>"></i> <?= $upperMenuArray['logoff']['title'] ?></a></li>
+                            <?php } ?>
                         </ul>
                     </li>
                 </ul>
